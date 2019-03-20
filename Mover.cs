99f9fa9f219031ed0a0fe5b8d1c2242026851d0a
lionboy1 <39709290+lionboy1@users.noreply.github.com>
2019-03-20 12:13:02 -0400

    Navmesh agent navigation
    Navigating  AI agents or player via point and click.

---
+++ Mover.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+//Moving agents on a navmesh
+public class Mover : MonoBehaviour{
+	[SerializeField] Transform target;
+	void Update(){
+		if(Inout.GetMouseButonDown(0)){
+			MoveToCursor();
+		}
+	}
+	
+	private void MoveToCursor(){
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosiiton);//struct representing an infinite line at origin and going in some direction.
+		RaycastHit hit; // struct to get information from the ray.
+		bool hasHit = Physics.Raycast(ray, out hit);// the ray has to go out and hit something.
+		if (hasHit)
+		{
+			GetCOmponent<NavMeshAgent>().destination = hit.point;//grab the navmesh component and set the destination to where the hit point was.
+		}
+	}
+}
+	
