95525d010d6c72c527a3fefbc9ae568def2b54af
lionboy1 <39709290+lionboy1@users.noreply.github.com>
2019-03-25 12:43:07 -0400

    Flee and AI script - unmodified
    Udemy penny borrow flee and AI script to control flee - unmodified

---
+++ AIControl.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIControl : MonoBehaviour {
+
+	GameObject[] goalLocations;
+	NavMeshAgent agent;
+	Animator anim;
+	float speedMult;
+	float detectionRadius = 20;
+	float fleeRadius = 10;
+
+	void ResetAgent()
+	{
+		speedMult = Random.Range(0.1f,1.5f);
+		agent.speed = 2 * speedMult;
+		agent.angularSpeed = 120;
+		anim.SetFloat("speedMult", speedMult);
+		anim.SetTrigger("isWalking");
+		agent.ResetPath();
+	}
+
+	public void DetectNewObstacle(Vector3 position)
+	{
+		if(Vector3.Distance(position, this.transform.position) < detectionRadius)
+		{
+			Vector3 fleeDirection = (this.transform.position - position).normalized;
+			Vector3 newgoal = this.transform.position + fleeDirection * fleeRadius;
+
+			NavMeshPath path = new NavMeshPath();
+        	agent.CalculatePath(newgoal, path);
+
+			if(path.status != NavMeshPathStatus.PathInvalid)
+			{
+				agent.SetDestination(path.corners[path.corners.Length - 1]);
+				anim.SetTrigger("isRunning");
+				agent.speed = 10;
+				agent.angularSpeed = 500;
+			}
+		
+		}
+	}
+
+
+	// Use this for initialization
+	void Start () {
+		goalLocations = GameObject.FindGameObjectsWithTag("goal");
+		agent = this.GetComponent<NavMeshAgent>();
+		agent.SetDestination(goalLocations[Random.Range(0,goalLocations.Length)].transform.position);
+		anim = this.GetComponent<Animator>();
+		anim.SetFloat("wOffset", Random.Range(0,1));
+		ResetAgent();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(agent.remainingDistance < 1)
+		{
+			ResetAgent();
+			agent.SetDestination(goalLocations[Random.Range(0,goalLocations.Length)].transform.position);
+		}		
+	}
+}
---
+++ DropCylinder.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropCylinder : MonoBehaviour {
+
+	public GameObject obstacle;
+	GameObject[] agents;
+	// Use this for initialization
+	void Start () {
+		agents = GameObject.FindGameObjectsWithTag("agent");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown(0)) {
+			RaycastHit hitInfo;
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			if (Physics.Raycast(ray.origin, ray.direction, out hitInfo))
+			{
+				Instantiate(obstacle,hitInfo.point,obstacle.transform.rotation);
+				foreach(GameObject a in agents)
+				{
+					a.GetComponent<AIControl>().DetectNewObstacle(hitInfo.point);
+				}
+			}
+		}
+	}
+}
