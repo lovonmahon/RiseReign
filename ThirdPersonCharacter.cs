076e0cf5c2cfdbe4991df07639bcd214d367ac93
lionboy1 <39709290+lionboy1@users.noreply.github.com>
2019-03-29 16:09:07 -0400

    Update ThirdPersonCharacter.cs

--- ThirdPersonCharacter.cs
+++ ThirdPersonCharacter.cs
@@ -29,6 +29,8 @@ namespace UnityStandardAssets.Characters.ThirdPerson
 		Vector3 m_CapsuleCenter;
 		CapsuleCollider m_Capsule;
 		bool m_Crouching;
+		bool isDead;//checks if player is dead.
+		bool damaged;//checks if player is damaged.
 
         public float health = 100;
         public float hunger = 100;
@@ -85,6 +87,49 @@ namespace UnityStandardAssets.Characters.ThirdPerson
         }
 
 
+		public void TakeDamage (int amount)
+    {
+        // Set the damaged flag so the screen will flash.
+        damaged = true;
+
+        // Reduce the current health by the damage amount.
+        currentHealth -= amount;
+
+        // Set the health bar's value to the current health.
+        healthSlider.value = currentHealth;
+
+        // Play the hurt sound effect.
+        playerAudio.Play ();
+
+        // If the player has lost all it's health and the death flag hasn't been set yet...
+        if(currentHealth <= 0 && !isDead)
+        {
+            // ... it should die.
+            Death ();
+        }
+    }
+
+
+    		void Death ()
+    {
+        // Set the death flag so this function won't be called again.
+        isDead = true;
+
+        // Turn off any remaining shooting effects.
+        playerShooting.DisableEffects ();
+
+        // Tell the animator that the player is dead.
+        anim.SetTrigger ("Die");
+
+        // Set the audiosource to play the death clip and play it (this will stop the hurt sound from playing).
+        playerAudio.clip = deathClip;
+        playerAudio.Play ();
+
+        // Turn off the movement and shooting scripts.
+        playerMovement.enabled = false;
+        playerShooting.enabled = false;
+    }       
+		
 		public void Move(Vector3 move, bool crouch, bool jump)
 		{
 
