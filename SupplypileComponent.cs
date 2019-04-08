fe65be95d52d9b05ece95a0e2362943c1d871b08
lionboy1 <39709290+lionboy1@users.noreply.github.com>
2019-04-08 16:46:05 -0400

    Update SupplypileComponent.cs

--- SupplypileComponent.cs
+++ SupplypileComponent.cs
@@ -7,5 +7,21 @@ public class SupplyPileComponent : MonoBehaviour
 	public int numLogs; // makes firewood
 	public int numFirewood; // what we want to make
 	public int numOre; // makes tools
+	public int nails;
+	public int trap;
+	public int coins;
+	public int berries;
+	public int RawFlour;
+	public int Flour;
+	public int fishingRod;
+	public int Planks;
+	public int Meat;
+	public int Hammer;
+	public int Fish;
+	public int cocoaBalls;
+	public int cocoaTea;
+	public int Weapon;
+	public int Wheat;
+	 
 	
 }
