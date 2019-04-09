753007c4da5706e7cfb8fe5648090b10d2deabde
lionboy1 <39709290+lionboy1@users.noreply.github.com>
2019-04-09 16:15:38 -0400

    Update Enemy.cs

--- Enemy.cs
+++ Enemy.cs
@@ -9,7 +9,18 @@ public abstract class Enemy : MonoBehaviour, IGoap {
 	public BoxCollider boxCollider;
 	public PlayerMovement player;
 	public BackpackComponent backpack;
+	
+    	public float sinkSpeed = 2.5f;              // The speed at which the enemy sinks through the floor when dead.
+    	public int scoreValue = 10;                 // The amount added to the player's score when the enemy dies.
+    	public AudioClip deathClip;                 // The sound to play when the enemy dies.
 
+	  
+    	AudioSource enemyAudio;                     // Reference to the audio source.
+    	ParticleSystem hitParticles;                // Reference to the particle system that plays when the enemy is damaged.
+    	CapsuleCollider capsuleCollider;            // Reference to the capsule collider.
+    	bool isDead;                                // Whether the enemy is dead.
+    	bool isSinking;                             // Whether the enemy has started sinking through the floor.
+	
 	public int health;
 	public int strength;
 	public int speed;
@@ -45,7 +56,70 @@ public abstract class Enemy : MonoBehaviour, IGoap {
 		}
 	}
 
-	public abstract void passiveRegen();
+	public virtual void passiveRegen();
+	
+   public virtual void TakeDamage (int amount, Vector3 hitPoint)
+    {
+        // If the enemy is dead...
+        if(isDead)
+            // ... no need to take damage so exit the function.
+            return;
+
+        // Play the hurt sound effect.
+        enemyAudio.Play ();
+
+        // Reduce the current health by the amount of damage sustained.
+        currentHealth -= amount;
+            
+        // Set the position of the particle system to where the hit was sustained.
+        hitParticles.transform.position = hitPoint;
+
+        // And play the particles.
+        hitParticles.Play();
+
+        // If the current health is less than or equal to zero...
+        if(currentHealth <= 0)
+        {
+            // ... the enemy is dead.
+            Death ();
+        }
+    }
+
+
+    public virtual void Death ()
+    {
+        // The enemy is dead.
+        isDead = true;
+
+        // Turn the collider into a trigger so shots can pass through it.
+        capsuleCollider.isTrigger = true;
+
+        // Tell the animator that the enemy is dead.
+        anim.SetTrigger ("Dead");
+
+        // Change the audio clip of the audio source to the death clip and play it (this will stop the hurt clip playing).
+        enemyAudio.clip = deathClip;
+        enemyAudio.Play ();
+    }
+
+
+    public virtual void StartSinking ()
+    {
+        // Find and disable the Nav Mesh Agent.
+        GetComponent <NavMeshAgent> ().enabled = false;
+
+        // Find the rigidbody component and make it kinematic (since we use Translate to sink the enemy).
+        GetComponent <Rigidbody> ().isKinematic = true;
+
+        // The enemy should no sink.
+        isSinking = true;
+
+        // Increase the score by the enemy's score value.
+        ScoreManager.score += scoreValue;
+
+        // After 2 seconds destory the enemy.
+        Destroy (gameObject, 2f);
+    }
 
 	public HashSet<KeyValuePair<string, object>> getWorldState(){
 		HashSet<KeyValuePair<string, object>> worldData = new HashSet<KeyValuePair<string, object>> ();
