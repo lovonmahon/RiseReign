b5bf917b208e44538c9d6c4ec75c7562af114c16
lionboy1 <39709290+lionboy1@users.noreply.github.com>
2019-05-15 16:43:20 -0400

    Update Worker.cs
    added 'Line of Sight' code from beginner ai penny udemy video

--- Worker.cs
+++ Worker.cs
@@ -4,16 +4,25 @@ using System.Collections.Generic;
 using UnityEngine.AI;
 //To create different worker classes, rename this class to suit.
 
-public class Worker : MonoBehaviour, IGoap
+public abstract class Worker : MonoBehaviour, IGoap
 {
 	NavMeshAgent agent;
+	Animator anim;
 	Vector3 previousDestination;
 	Inventory inv;
 	public Inventory stockpile;
+	[SerializeField] float rotationSpeed = 2.0f;
+	[SerializeField] float visDist = 20.0f;
+	[SerializeField] float visAngle = 30.0f;
+	[SerializeField] float shootDist = 20.0f;
+	[SerializeField] float throwDist = 10.0f;
+	[SerializeField] float meleeDist = 1.0f;
+	//String state = "WORK";
 
 	void Start()
 	{
 		agent = this.GetComponent<NavMeshAgent>();
+		anim = this.GetComponent<Animator>();
 		inv = this.GetComponent<Inventory>();
 		stockpile = GetComponent<Inventory>();
 	}
@@ -73,7 +82,7 @@ public class Worker : MonoBehaviour, IGoap
 	}
 
 
-	public bool MoveAgent(GoapAction nextAction) {
+	public virtual bool MoveAgent(GoapAction nextAction) {
 		
 		//if we don't need to move anywhere
 		if(previousDestination == nextAction.target.transform.position)
@@ -97,8 +106,18 @@ public class Worker : MonoBehaviour, IGoap
 		if(agent.hasPath)
 		{
 			Vector3 toTarget = agent.steeringTarget - this.transform.position;
-         	float turnAngle = Vector3.Angle(this.transform.forward,toTarget);
-         	agent.acceleration = turnAngle * agent.speed;
+         		float turnAngle = Vector3.Angle(this.transform.forward,toTarget);
+         		agent.acceleration = turnAngle * agent.speed;
+		}
+		//code to rotate character to look at player taken from 'line of sight' in penny udemy.
+		if(toTarget.magnitude < visDist && turnAngle < visAngle)
+		{			
+			toTarget.y = 0;
+
+			this.transform.rotation = Quaternion.Slerp(this.transform.rotation,
+										Quaternion.LookRotation(direction), 
+										Time.deltaTime * rotationSpeed);		
+
 		}
 	}
 
