5377d2bcd663ede87da2744977d0d0c8339c5468
lionboy1 <39709290+lionboy1@users.noreply.github.com>
2019-05-19 06:51:23 -0400

    Create AttackAction.cs
    Humanoid attack

---
+++ AttackAction.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackAction : GoapAction {
+
+	[SerializeField] float timeBetweenAttack = 1.0f;
+    float timeSinceLastAttack = 0;
+    public Animator anim;
+    bool attacked = false;
+	//Try caching the animator if performance suffers from initializing it in the perform().
+	/*void Awake(){
+		anim = GameObject.GetComponent<Animator>();
+	}*/
+
+	void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
+    
+    public AttackAction(){
+        addPrecondition("damagePlayer", false);
+		addEffect ("doJob", true);
+		//cost = 1.0f;
+        name = "AttackAction";
+	}
+
+	void Update()
+    {
+        timeSinceLastAttack += Time.deltaTime; // checks when last attack occurred.
+    }
+    
+    public override void reset() {
+		attacked = false;
+		target = null;
+	}
+
+	public override bool isDone(){
+		return attacked;
+	}
+
+	public override bool requiresInRange(){
+		return true;
+	}
+
+	public override bool checkProceduralPrecondition(GameObject agent){
+		target = GameObject.FindWithTag("Player");
+		//return target != null;
+        return true;
+	}
+
+	public override bool perform(GameObject agent){
+		//Alligator gator = agent.GetComponent<Alligator> ();
+		//gator.stamina -= (500 - cost);
+		//Throttle attack
+        if(timeSinceLastAttack > timeBetweenAttack)
+        {
+            anim.SetTrigger("attack");
+            timeSinceLastAttack = 0;//reset attack time.
+        }        
+		attacked = true;
+		return attacked;
+        return true;
+	}
+}
