4ff095f08b0e7352f8bedcb2efd409485b09d5e4
lionboy1 <39709290+lionboy1@users.noreply.github.com>
2019-05-20 15:26:05 -0400

    Update Worker.cs
    added meleeDist for agent stopping range peforming performing next action.

--- Worker.cs
+++ Worker.cs
@@ -93,7 +93,7 @@ public abstract class Worker : MonoBehaviour, IGoap
 		
 		agent.SetDestination(nextAction.target.transform.position);
 		
-		if (agent.hasPath && agent.remainingDistance < meleeDist) {
+		if (agent.hasPath && agent.remainingDistance <= meleeDist) {
 			nextAction.setInRange(true);
 			previousDestination = nextAction.target.transform.position;
 			return true;
