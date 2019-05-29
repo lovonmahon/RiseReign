99538c0981185f2dbcf14af3753b00502669364d
lionboy1 <39709290+lionboy1@users.noreply.github.com>
2019-05-29 16:14:01 -0400

    Update PlayerHealth.cs
    Removed health block from ThirdPersonController and added here.

--- PlayerHealth.cs
+++ PlayerHealth.cs
@@ -7,12 +7,21 @@ public class PlayerHealth : MonoBehaviour
     public int startingHealth = 100;                            // The amount of health the player starts the game with.
     public int currentHealth;                                   // The current health the player has.
     public Slider healthSlider;                                 // Reference to the UI's health bar.
+    public Slider hunger;                                       // Reference to the UI's hunger bar.
+    public Slider thirst;                                       // Reference to the UI's thirst bar.
+    
     public Image damageImage;                                   // Reference to an image to flash on the screen on being hurt.
     public AudioClip deathClip;                                 // The audio clip to play when the player dies.
     public float flashSpeed = 5f;                               // The speed the damageImage will fade at.
     public Color flashColour = new Color(1f, 0f, 0f, 0.1f);     // The colour the damageImage is set to, to flash.
 
-
+    public float hunger = 0;
+    public float thirst = 0;
+    public float thirstRate;
+    public float hungerRate;
+    public float deathRate;
+    
+    
     Animator anim;                                              // Reference to the Animator component.
     AudioSource playerAudio;                                    // Reference to the AudioSource component.
     ThirdPersonCHaracter playerMovement;                              // Reference to the player's movement.
@@ -36,6 +45,26 @@ public class PlayerHealth : MonoBehaviour
 
     void Update ()
     {
+        hunger.value = hunger;
+        thirst.value = thirst;
+       
+        hunger = hunger - (hungerRate * Time.deltaTime);
+        thirst = thirst - (thirstRate * Time.deltaTime);
+        
+        if(hunger <= 0 || thirst <= 0)
+        {
+            health = health - (deathRate * Time.deltaTime);
+        }
+        
+        if(hunger <= 0)
+        {
+           hunger = 0;
+        }
+        if (thirst <= 0)
+        {
+           thirst = 0;
+        }
+        
         // If the player has just been damaged...
         if(damaged)
         {
