1b158bb832b03dbc9ebe6770de1afa5bf206d0e2
lionboy1 <39709290+lionboy1@users.noreply.github.com>
2019-06-05 14:42:05 -0400

    Update EvadeAction.cs

--- EvadeAction.cs
+++ EvadeAction.cs
@@ -9,7 +9,9 @@ public class EvadeAction : GoapAction {
     	Animator anim;
     	bool isHiding = false;
 	bool avoid = false;
-	Transform healingSpot;
+	float threatDistance = 20.0f;
+	public GameObject healingSpots[];//array of places to hide/heal.
+	int currentHealingSpot = 0;
 	//Try caching the animator if performance suffers from initializing it in the perform().
 	/*void Awake(){
 		anim = GameObject.GetComponentInChildren<Animator>();
@@ -20,7 +22,8 @@ public class EvadeAction : GoapAction {
     {
         anim = gameObject.GetComponentInChildren<Animator>();
 		health = this.GetComponent<EnemyHealth>();
-		healingSpot = gameObject.GetComponent<safeHavenComponent>();
+		healingSpots = gameObject.GetComponent<safeHavenComponent>();
+		currentHealingSpot = FindClosestSpot();
     }
     
     public EvadeAction(){
@@ -32,16 +35,33 @@ public class EvadeAction : GoapAction {
 
 	void Update()
     {
-        	if(target != null)
+        	float distance = Vector3.Distance(this.transform.position, target.transform.position);//calculate distance from transform to target.
+		if(target != null && distance < threatDistance )
 		{
 			//transform.LootAt(target);//always look at the target
 			if(avoid)
 			{
-				Vector3 targetCheck = transform.position - target.position;//check distance to the target
+				Vector3 targetCheck = transform.position - target.position;//second check distance to the target
 				Vector3 goToPosition = transform.position + targetCheck;//Calculate flee distance to move away from target threat.
 				agent.SetDestination(goToPosition);//Run!
 			}
 		}
+    }
+	
+    void FindCLosestSpot() //Based on Holistic3D tutorial 'AI: Finding the Closest Waypoint in Unity 5'
+    {
+	    if(healingSpots.Length == 0) return -1//if no spots are found return nothing.
+	    int closest = 0;//Otherwise set the closest spot to the first location in the array.
+	    float lastDist = Vector3.Distance(this.transform.position, healingSpots[0].transform.position);//returns the distance between transform and last waypoint.
+	    for(int i = 1, i < healingSpots.Length, i++)//Iterate over the remaining waypoints in the list to check which is closest to transform.
+	    {
+		  float thisDist = Vector3.Distance(this.transform.position, healingSpots[i].transform.position);
+		  if(lastDist > thisDist && i != currentHealingSpot)
+		  {
+			  closest = i;
+		  }		   
+	    }
+	    return closest; 
     }
     
     public override void reset() {
