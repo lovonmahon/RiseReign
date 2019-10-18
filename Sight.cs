f6babab236edfde5be13300a19cc1d6bf37d7a0e
lionboy1 <39709290+lionboy1@users.noreply.github.com>
2019-10-18 14:02:12 -0400

    Update Sight.cs
    Added namespace and cleaned up ray code

--- Sight.cs
+++ Sight.cs
@@ -2,55 +2,68 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Sight : MonoBehaviour {
-
-		//Try caching the animator if performance suffers from initializing it in the perform().
-	/*void Awake(){
-		anim = GameObject.GetComponent<Animator>();
-	}*/
-
-public Ray l_ray;
-public Ray r_ray;
-public Ray f_ray;
-	
-public float goatDestination;
-	
-    void Start()
+namespace RiseReign
+{    
+    public class Sight : MonoBehaviour
     {
-        
-    }
     
-  void Update()
-    {
-        	RaycastHit hit;
-
-          float theDist;
-          Vector3 forwardDist = transform.position, transform.TransformDirection(Vector3.forward) * 20;
-          Vector3 leftDist = transform.position, transform.TransformDirection(Vector3.left) * 20;
-          Vector3 rightdDist = transform.position, transform.TransformDirection(Vector3.right) * 20;
-          if(Physics.Raycast(transform.position, (forwardDist), out hit)
-{
-	if(hit.collider.tag == "player" && health > 30)
-	{
-		theDist = hit.distance;
-	}
-	
-}
-
-if(Physics.Raycast(transform.position, (leftDist), out hit)
-{
-	if(hit.collider.tag == "player" && health > 30)
-	{
-		theDist = hit.distance;
-	}
-}
-
-if(Physics.Raycast(transform.position, (rightdDist), out hit)
-{
-	if(hit.collider.tag == "player" && health > 30)
-	{
-		theDist = hit.distance;
-	}
+        //Try caching the animator if performance suffers from initializing it in the perform().
+        /*void Awake(){
+            anim = GameObject.GetComponent<Animator>();
+        }*/
+    
+    
+    
+        public float goatDestination;
+        public bool detected = false;
+    
+        void Start()
+        {
+    
+        }
+    
+        void Update()
+        {
+             RaycastHit hit;
+    
+             float theDist;
+             Vector3 forwardDist = transform.TransformDirection(Vector3.forward) * 20;
+             Vector3 leftDist = transform.TransformDirection(Vector3.left) * 20;
+             Vector3 rightdDist = transform.TransformDirection(Vector3.right) * 20;
+    
+             if(Physics.Raycast(transform.position, (forwardDist), out hit))
+    	     {
+    	        if(hit.collider.tag == "player" && health > 30)
+    	        {
+    		        theDist = hit.distance;
+                    detected = true;
+    	        }
+             }	
+    
+            if(Physics.Raycast(transform.position, (leftDist), out hit))
+            {
+            	if(hit.collider.tag == "player" && health > 30)
+            	{
+            		theDist = hit.distance;
+                    detected = true;
+            	}
+            }
+    
+            if(Physics.Raycast(transform.position, (rightdDist), out hit))
+            {
+            	if(hit.collider.tag == "player" && health > 30)
+            	{
+            		theDist = hit.distance;
+                    detected = true;
+            	}
+            }
+    
+            else
+            {
+                detected = false;
+            }
+        }
+    }
 }
 
 
