46fab4762b5fd330243c2ef1be2339c8ac68561e
lionboy1 <39709290+lionboy1@users.noreply.github.com>
2019-10-30 15:09:23 -0400

    Create SwitchControl.cs
    Controller to switch between player and various vehicles

---
+++ SwitchControl.cs
@@ -0,0 +1,43 @@
+using System;
+using UnityEngine;
+
+namespace RiseReign
+{
+    
+    public GameObject vehicle;
+    public GameObject vehicleCam;
+    public GameObject player;
+    public GameObject playerStartPos;
+    
+    public class SwitchControl : MonoBehaviour
+    {
+        void Start(){}
+
+        void Update()
+        {
+            //Vehicle mode
+            if(Input.GetKey("1"))
+            {
+                vehicle.GetComponent<RigidBody>().isKinematic = false;
+                vehicle.GetComponent<BoatController>().enabled = true;
+                vehicleCam.SetActive(true);
+
+                //player.SetActive(false); this completely hides player
+
+                player.GetComponent<ThirsPersonController>().enabled(false);//this only disables player control but character still visible
+            }
+
+            if(Input.GetKey("2"))
+            {
+                vehicle.GetComponent<RigidBody>().isKinematic = true;
+                vehicle.GetComponent<BoatController>().enabled = false;
+                vehicleCam.SetActive(false);
+
+                //player.SetActive(true);
+
+                player.GetComponent<ThirsPersonController>().enabled(true);
+            }
+    }
+}
+}
+
