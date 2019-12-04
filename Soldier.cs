bcfddf3bc0d76e245d4568c14e7fb0c80584e527
lionboy1 <39709290+lionboy1@users.noreply.github.com>
2019-12-04 16:37:56 -0500

    Create Soldier.cs

---
+++ Soldier.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Soldier : Worker
+{
+	/**
+	 * The Soldier's main goal is civil order and combat but can engage in other 
+	 * activities when needed.
+	 */
+	public override HashSet<KeyValuePair<string,object>> CreateGoalState () {
+		HashSet<KeyValuePair<string,object>> goal = new HashSet<KeyValuePair<string,object>> ();
+		
+		goal.Add(new KeyValuePair<string, object>("patrol", true ));
+		goal.Add(new KeyValuePair<string, object>("investigate", true ));
+		goal.Add(new KeyValuePair<string, object>("escort", true ));
+		goal.Add(new KeyValuePair<string, object>("rest", true ));
+		goal.Add(new KeyValuePair<string, object>("fight", true ));
+		goal.Add(new KeyValuePair<string, object>("flee", true ));
+		return goal;
+	}
+
+}
