fac80edfc638f062c3f0f61c90b984e4495d5f84
lionboy1 <39709290+lionboy1@users.noreply.github.com>
2019-12-05 13:41:25 -0500

    Update Knight.cs
    All combat units will inherit from the Military class

--- NPC/Knight.cs
+++ NPC/Knight.cs
@@ -2,22 +2,22 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
-public class Knight : Worker {
-
-	GameObject player;
-	
-    void Start ()
-    {
-		//player = gameObject.FindWithTag("Player").GetComponent<ThirdPersonCharacter>();
-	}
-
-	public override HashSet<KeyValuePair<string, object>> CreateGoalState(){
-		HashSet<KeyValuePair<string, object>> goal = new HashSet<KeyValuePair<string, object>> ();
-		goal.Add (new KeyValuePair<string, object> ("damagePlayer", true));
-		goal.Add (new KeyValuePair<string, object> ("stayAlive", true));
-        goal.Add (new KeyValuePair<string, object> ("runAway", true));
+public class Knight : Military
+{
+	/**
+	 * The Knight's main goal is civil order and leadership but can engage in other 
+	 * activities when needed.
+	 */
+	public override HashSet<KeyValuePair<string,object>> CreateGoalState () {
+		HashSet<KeyValuePair<string,object>> goal = new HashSet<KeyValuePair<string,object>> ();
+		
+		goal.Add(new KeyValuePair<string, object>("patrol", true ));
+		goal.Add(new KeyValuePair<string, object>("investigate", true ));
+		goal.Add(new KeyValuePair<string, object>("escort", true ));
+		goal.Add(new KeyValuePair<string, object>("rest", true ));
+		goal.Add(new KeyValuePair<string, object>("fight", true ));
+		goal.Add(new KeyValuePair<string, object>("flee", true ));
 		return goal;
 	}
 
-	
 }
