1a2b2eaeee6ea560977e41d88dacb7773f6a9c9d
lionboy1 <39709290+lionboy1@users.noreply.github.com>
2019-12-05 14:27:04 -0500

    Create Inventory.cs
    inventory for individual nps/buildings

---
+++ Inventory/Inventory.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+//General inventory for all
+namespace RiseReign
+{
+	public class Inventory : Monobehaviour
+	{
+		public int fishingRod = 0;
+		public int Wheat = 0;
+		public int Bakes = 0;
+		public int Flour = 0;
+		public int RawFlour = 0;
+		public int Meat = 0;
+		public int Fish = 0;
+		public int Herbs = 0;
+		public int Berries = 0;
+		public int Manure = 0;
+		public int Hammer = 0;
+		public int Planks = 0;
+		public int Weapon = 0;
+		public int Nails = 0;
+		public int Coins = 0;
+		public int Trap = 0;
+		public int CaughtAnimal = 0;
+		public int cocoaBalls = 0;
+		public int CocoaTea = 0;
+	}
+}
