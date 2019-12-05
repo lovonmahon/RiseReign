400e7ce2d730bc647a4a1ecc7c904baa4b939e61
lionboy1 <39709290+lionboy1@users.noreply.github.com>
2019-12-05 13:42:36 -0500

    Update Soldier.cs
    Inherits from the military class.  Moving to military directory

--- NPC/Soldier.cs
+++ NPC/Soldier.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
-public class Soldier : Worker
+public class Soldier : Military
 {
 	/**
 	 * The Soldier's main goal is civil order and combat but can engage in other 
