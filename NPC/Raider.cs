45c69d92b5b064789442c66aa5838c493e599834
lionboy1 <39709290+lionboy1@users.noreply.github.com>
2019-12-05 13:46:36 -0500

    Update Raider.cs
    moving to military directory. Inherits from military

--- NPC/Raider.cs
+++ NPC/Raider.cs
@@ -2,53 +2,24 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
-public class Raider : Enemy {
-
-	// Use this for initialization
-	void Start () {
-		stamina = 100f;
-		health = 50;
-		speed = 20;
-		strength = 10;
-		regenRate = .5f;
-		maxStamina = 100f;
-
-		terminalSpeed = speed / 10;
-		initialSpeed = (speed / 10) / 2;
-		acceleration = (speed / 10) / 4;
-
-		animator = GetComponent<Animator> ();
-		player = GameObject.FindWithtag("Player").GetComponent<PlayerMovement>();
-
-	}
-
-	public override void passiveRegen(){
-		stamina += regenRate;
-	}
-
-	public override HashSet<KeyValuePair<string, object>> createGoalState(){
-		HashSet<KeyValuePair<string, object>> goal = new HashSet<KeyValuePair<string, object>> ();
-		goal.Add (new KeyValuePair<string, object> ("damagePlayer", true));
-		goal.Add (new KeyValuePair<string, object> ("stayAlive", true));
-		goal.Add(new KeyValuePair<string, object>("hasFishingRod", true ));
-		goal.Add(new KeyValuePair<string, object>("hasWheat", true ));
-		goal.Add(new KeyValuePair<string, object>("hasBakes", true ));
-		goal.Add(new KeyValuePair<string, object>("hasBakingFlour", true ));
-		goal.Add(new KeyValuePair<string, object>("hasRawFlour", true ));
-		goal.Add(new KeyValuePair<string, object>("hasMeat", true ));
-		goal.Add(new KeyValuePair<string, object>("hasFish", true ));
-		goal.Add(new KeyValuePair<string, object>("hasHerbs", true ));
-		goal.Add(new KeyValuePair<string, object>("hasBerries", true ));
-		goal.Add(new KeyValuePair<string, object>("hasManure", true ));
-		goal.Add(new KeyValuePair<string, object>("hasHammer", true ));
-		goal.Add(new KeyValuePair<string, object>("hasPlanks", true ));
-		goal.Add(new KeyValuePair<string, object>("hasWeapon", true ));
-		goal.Add(new KeyValuePair<string, object>("hasNails", true ));
-		goal.Add(new KeyValuePair<string, object>("hasCoins", true ));
-		goal.Add(new KeyValuePair<string, object>("hasTrap", true ));
-		goal.Add(new KeyValuePair<string, object>("hasAnimalCaught", true ));
-		goal.Add(new KeyValuePair<string, object>("hasCocoaBalls", true ));
-		goal.Add(new KeyValuePair<string, object>("hasCocoaTea", true ));
+public class Raider : Military
+{
+	/**
+	 * The Raider's main goal is ruthless stealing of goods but can engage in other 
+	 * activities when needed.
+	 */
+	public override HashSet<KeyValuePair<string,object>> CreateGoalState () {
+		HashSet<KeyValuePair<string,object>> goal = new HashSet<KeyValuePair<string,object>> ();
+		
+		goal.Add(new KeyValuePair<string, object>("steal", true ));
+		goal.Add(new KeyValuePair<string, object>("investigate", true ));
+		goal.Add(new KeyValuePair<string, object>("escort", true ));
+		goal.Add(new KeyValuePair<string, object>("rest", true ));
+		goal.Add(new KeyValuePair<string, object>("fight", true ));
+		goal.Add(new KeyValuePair<string, object>("flee", true ));
+		goal.Add(new KeyValuePair<string, object>("embark", true ));//use for boats/ships
+		goal.Add(new KeyValuePair<string, object>("disembark", true ));//use for boats/ships
 		return goal;
 	}
+
 }
