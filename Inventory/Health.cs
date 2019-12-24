2d21f506833de2bf08214687617d2432869a01c5
lionboy1 <39709290+lionboy1@users.noreply.github.com>
2019-12-24 14:19:44 -0500

    Create Health.cs

---
+++ Inventory/Health.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RiseReign{
+	
+	public class Health : MonoBehaviour
+	{
+		public int amount = 100;
+
+		void Start()
+		{
+			//
+		}
+
+		void Update()
+		{
+			//
+		}
+	}
+}
