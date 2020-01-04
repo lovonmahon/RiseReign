e29e87abe67a930602dfc350c58b8028c3c31b37
lionboy1 <39709290+lionboy1@users.noreply.github.com>
2020-01-03 22:59:30 -0500

    Update Soldier.cs

--- NPC/Military/Soldier.cs
+++ NPC/Military/Soldier.cs
@@ -8,16 +8,6 @@ public class Soldier : Military
 	 * The Soldier's main goal is civil order and combat but can engage in other 
 	 * activities when needed.
 	 */
-	public override HashSet<KeyValuePair<string,object>> CreateGoalState () {
-		HashSet<KeyValuePair<string,object>> goal = new HashSet<KeyValuePair<string,object>> ();
-		
-		goal.Add(new KeyValuePair<string, object>("patrol", true ));
-		goal.Add(new KeyValuePair<string, object>("investigate", true ));
-		goal.Add(new KeyValuePair<string, object>("escort", true ));
-		goal.Add(new KeyValuePair<string, object>("rest", true ));
-		goal.Add(new KeyValuePair<string, object>("fight", true ));
-		goal.Add(new KeyValuePair<string, object>("flee", true ));
-		return goal;
-	}
+	
 
 }
