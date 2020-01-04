0c930916480826c6860a67305f6bff2337d4ce9b
lionboy1 <39709290+lionboy1@users.noreply.github.com>
2020-01-03 23:00:19 -0500

    Update AttackAction.cs

--- Aggressive_Actions/AttackAction.cs
+++ Aggressive_Actions/AttackAction.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+using RiseReign;
 
 public class AttackAction : GoapAction {
 
@@ -8,7 +10,7 @@ public class AttackAction : GoapAction {
     public Animator anim;
     bool attacked = false;
     Sight sight;
-    NavmeshAgent agent;
+    NavMeshAgent agent;
 	//Try caching the animator if performance suffers from initializing it in the perform().
 	/*void Awake(){
 		anim = GameObject.GetComponent<Animator>();
@@ -17,23 +19,25 @@ public class AttackAction : GoapAction {
 	void Start()
     {
         anim = gameObject.GetComponentInChildren<Animator>();
-	sight = gameObject.GetComponent<Sight>();
-	agent = this.GetComponent<NAvmeshAgent>();
+	    sight = gameObject.GetComponent<Sight>();
+	    agent = this.GetComponent<NavMeshAgent>();
     }
     
     public AttackAction(){
-        addEffect ("damagePlayer", true);
+        addPrecondition("canSeePlayer", true);
+		addEffect ("fight", true);
+		//addEffect ("doJob", true);
         name = "Attack the player";
 	}
-
-	void Update()
+    //Deal with look at later.
+	/*void Update()
     {
         	if(target != null)
 		{
-			transform.LootAt(target);//always look at the target
+			this.transform.LookAt(target);//always look at the target
 			//timeSinceLastAttack += Time.deltaTime; // checks when last attack occurred.
 		}
-    }
+    }*/
     
     public override void reset() {
 		attacked = false;
@@ -50,11 +54,13 @@ public class AttackAction : GoapAction {
 
 	public override bool checkProceduralPrecondition(GameObject agent){
 		target = GameObject.FindWithTag("Player");
-		if( sight.m_canSeePlayer == true)
 		{
 			if( target != null )
 			{
-				return true;
+                if( sight.m_canSeePlayer == true)
+				{
+					return true;
+				}
 			}	
 		}
 		//return target != null;
@@ -62,15 +68,15 @@ public class AttackAction : GoapAction {
 	}
 
 	public override bool perform(GameObject agent){
-		anim.SetTrigger("attack");
-		if ( target.isBlocking )
+		anim.SetBool("farm", true);
+		Debug.Log("Attacking");
+		//anim.SetTrigger("attack");
+		/*if ( target.isBlocking )
 		{
 			transform.RotateAround( sight.dirToTarget, Vector3.up, 90f * Time.deltaTime );//points to the targets vector 3 location to rotate around.
-		}	
-		//Alligator gator = agent.GetComponent<Alligator> ();
-		//gator.stamina -= (500 - cost);
+		}*/ //deal with blocking later.	
               
 		attacked = true;
-         	return true;
+        return true;
 	}
 }
