2e1243df48da5afb18e864e34dfbcc7d32cac2e1
lionboy1 <39709290+lionboy1@users.noreply.github.com>
2020-01-05 18:29:46 -0500

    Update Farmer.cs

--- NPC/Civilian/Farmer.cs
+++ NPC/Civilian/Farmer.cs
@@ -6,20 +6,6 @@ public class Farmer : Worker
 {
 	/**
 	 * The Farmer's main goal is to farm but can engage in other 
-	 * activities when needed.
-	 */
-	public override HashSet<KeyValuePair<string,object>> CreateGoalState () {
-		HashSet<KeyValuePair<string,object>> goal = new HashSet<KeyValuePair<string,object>> ();
-		
-		goal.Add(new KeyValuePair<string, object>("collectWater", true ));
-		goal.Add(new KeyValuePair<string, object>("harvest", true ));
-		goal.Add(new KeyValuePair<string, object>("plant", true ));
-		goal.Add(new KeyValuePair<string, object>("husbandry", true ));
-		goal.Add(new KeyValuePair<string, object>("processMeat", true ));
-		goal.Add(new KeyValuePair<string, object>("rest", true ));
-		goal.Add(new KeyValuePair<string, object>("fight", true ));
-		goal.Add(new KeyValuePair<string, object>("flee", true ));
-		return goal;
-	}
-
+	 * activities when needed. */
+	
 }
