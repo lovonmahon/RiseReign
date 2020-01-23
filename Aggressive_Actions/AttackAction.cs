332712ee4bdfb6c2b413ebd5e309f4574426d311
lionboy1 <39709290+lionboy1@users.noreply.github.com>
2020-01-23 15:21:41 -0500

    Update AttackAction.cs
    Added reference to transform, enabled the lookat() in update.

--- Aggressive_Actions/AttackAction.cs
+++ Aggressive_Actions/AttackAction.cs
@@ -22,6 +22,11 @@ public class AttackAction : GoapAction {
 	    sight = gameObject.GetComponent<Sight>();
 	    agent = this.GetComponent<NavMeshAgent>();
     }
+
+    void Update()
+    {
+    	if 
+    }
     
     public AttackAction(){
         addPrecondition("canSeePlayer", true);
@@ -59,6 +64,89 @@ public class AttackAction : GoapAction {
                 if( sight.m_canSeePlayer == true)
 				{
 					return true;
+				}
+			}	
+        	return false;
+	}
+
+	public override bool perform(GameObject agent){
+		anim.SetBool("farm", true);
+		Debug.Log("Attacking");
+		//anim.SetTrigger("attack");
+		/*if ( target.isBlocking )
+		{
+			transform.RotateAround( sight.dirToTarget, Vector3.up, 90f * Time.deltaTime );//points to the targets vector 3 location to rotate around.
+		}*/ //deal with blocking later.	
+              
+		attacked = true;
+        return true;
+	}
+}
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using RiseReign;
+
+public class AttackAction : GoapAction {
+
+	public float aggroDist = 2.0f;
+    public Animator anim;
+    bool attacked = false;
+    Sight sight;
+    NavMeshAgent agent;
+	//Try caching the animator if performance suffers from initializing it in the perform().
+	/*void Awake(){
+		anim = GameObject.GetComponent<Animator>();
+	}*/
+
+	void Start()
+    {
+        anim = gameObject.GetComponentInChildren<Animator>();
+	    sight = gameObject.GetComponent<Sight>();
+	    agent = this.GetComponent<NavMeshAgent>();
+    }
+    
+    public AttackAction(){
+        addPrecondition("canSeePlayer", true);
+		addEffect ("fight", true);
+		//addEffect ("doJob", true);
+        name = "Attack the player if I'm healthy enough";
+	}
+    //Deal with look at later.
+	void Update()
+    {
+        	if(target != null)
+		{
+			this.transform.LookAt(target);//always look at the target
+			//timeSinceLastAttack += Time.deltaTime; // checks when last attack occurred.
+		}
+    }
+    
+    public override void reset() {
+		attacked = false;
+		target = null;
+	}
+
+	public override bool isDone(){
+		return attacked;
+	}
+
+	public override bool requiresInRange(){
+		return true;
+	}
+
+	public override bool checkProceduralPrecondition(GameObject agent){
+		target = GameObject.FindWithTag("Player").Getcomponent<Transform>();
+		float targetDist = target.position - this.transform.position;
+			if( target != null )
+			{
+                if( sight.m_canSeePlayer == true)
+				{
+					if( aggroDist > targetDist)
+					{
+						return true;
+					}	
 				}
 			}	
         	return false;
