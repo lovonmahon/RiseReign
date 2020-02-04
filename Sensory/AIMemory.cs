3702cf3c42eec6dbf3db38514988ce60e5e0a873
lionboy1 <39709290+lionboy1@users.noreply.github.com>
2020-02-04 15:34:57 -0500

    Create AIMemory.cs

---
+++ Sensory/AIMemory.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+//Learned from RPG Core
+
+public class AIMemory : MonoBehaviour
+{
+    [SerializeField] float suspicionTime = 10f;
+    float timeSinceLastSawPlayer = Math.Infinity;
+    Signt m_sight;
+    public bool alert = false;
+
+    //If the player is spotted, timeSinceLastSawPlayer = 0;
+    
+    void Update()
+    {
+        if( m_sight.m_canSeePlayer == true )
+        {
+            timeSinceLastSawPlayer = 0;
+        }
+        
+        if ( timeSinceLastSawPlayer < suspicionTime )
+        {
+            alert = true;
+        }
+       
+        alert = false;
+        timeSinceLastSawPlayer += Time.deltaTime;
+
+    }
+}
