198067b3d2ea5c3ce8b31d036305f3a8bbc616af
lionboy1 <39709290+lionboy1@users.noreply.github.com>
2020-02-13 16:23:10 -0500

    Update EvadeAction.cs

--- Avoid_Actions/EvadeAction.cs
+++ Avoid_Actions/EvadeAction.cs
@@ -32,7 +32,7 @@ public class EvadeAction : GoapAction {
     public EvadeAction(){
 		addPrecondition("canSeePlayer", true);
 		//addPrecondition ("flee", false);//Is this still needed?
-	    	addEffect ("isHiding", true);
+	    	addEffect ("needsToHide", true);
 		addEffect ("doJob", true);
         name = "Evade";
 	}
