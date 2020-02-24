c0bfec4e55f162becad22786763426f2f00cc637
lionboy1 <39709290+lionboy1@users.noreply.github.com>
2020-02-24 15:52:25 -0500

    Update DayNightCycle.cs
    Finished

--- DayNightCycle.cs
+++ DayNightCycle.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+//Based on SpeedTutor https://www.youtube.com/watch?v=MOuS2Wuntl8 (Based on twiik.net article Simplest possible day night cycle in Unity 5)
 namespace RiseReign
 {
 	public class DayNightCycle : MonoBehaviour
@@ -33,7 +35,29 @@ namespace RiseReign
 
 		void UpdateSun()
 		{
-			sun.transform.localRotation = Quaternion.Euler( ( currentTime * 360f ) - 90, 170, 0 );
+			
+			//Sun's rotation
+			sun.transform.localRotation = Quaternion.Euler( ( currentTime * 360f ) - 90, 170, 0 );//rotates 360, but uses a cutoff at 90x for horizon and 170y for same purpose.
+
+			//Control fading.
+			float intensityMultiplier = 1;
+
+			if( currentTime <= 0.23f || currentTime >= 0.75f ) //before sunrise or sunset.
+			{
+				intensityMultiplier = 0;
+			}
+
+			else if( currentTime <= 0.25f )
+			{
+				intensityMultiplier = Mathf.Clamp01(( currentTime - 0.23f ) * 1/ 0.02f ) );
+			}
+
+			else if( currentTime >= 0.73f ) 
+			{
+				intensityMultiplier = Mathf.Clamp01( 1 - (( currentTime - 0.73f ) * ( 1 / 0.02f) ));
+			}
+
+			sun.intensity = sunInitialIntensity * intentisyMultiplier;
 		}
 	}
 
