055d2af71da3cc612fd41c2acbcc859ce3ecece8
lionboy1 <39709290+lionboy1@users.noreply.github.com>
2020-03-10 16:58:23 -0400

    Create PatrolAction.cs
    WIP

---
+++ Curiosity_Actions/PatrolAction.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Waypoint idea borrowed from Holistic3d 'Simple waypoint  pathfinding with the Line of Sight AI'
+
+public class  PatrolAction : GoapAction {
+
+	public GameObject[] waypoints;
+	int currentWP= 0;
+	float accuracyWP = 5.pf;
+	bool completed = false;
+	//float startTime = 0;
+	//public float workDuration = 2; // seconds
+	
+	public PatrolAction () {
+		addPrecondition ("canSeePlayer", false); 
+		addEffect ("patrol", true);
+		name = "Patrol";
+	}
+	
+	public override void reset ()
+	{
+		completed = false;
+		startTime = 0;
+	}
+	
+	public override bool isDone ()
+	{
+		return completed;
+	}
+	
+	public override bool requiresInRange ()
+	{
+		return true; 
+	}
+	
+	public override bool checkProceduralPrecondition (GameObject agent)
+	{	
+		return true;
+	}
+	
+	public override bool perform (GameObject agent)
+	{
+		if( vector3.Distance(waypoints[currentWP].transform.position, transform.position) < accuracyWP)
+		{
+			currentWP++;
+			if(currentWP >= waypoints.Length)
+			{
+				currentWP = 0;
+			}
+			completed = true;
+		}
+		return true;
+	}
+	
+}
