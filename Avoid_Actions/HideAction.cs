6751242cb80b1993c5d1a37dc02cf441bc0debd0
lionboy1 <39709290+lionboy1@users.noreply.github.com>
2020-03-17 15:12:42 -0400

    Update HideAction.cs

--- Avoid_Actions/HideAction.cs
+++ Avoid_Actions/HideAction.cs
@@ -6,20 +6,19 @@ public class HideAction : GoapAction {
 
 	bool completed = false;
 	float startTime = 0;
-	public float workDuration = 10; // seconds
+	public float hideDuration = 30; // seconds
 	Animator anim;
-	LookAt lookTarget;
+	//LookAt lookTarget;
 	GameObject player;
 	
 	void Start()
 	{
 		anim = this.GetComponent<Animator>();
-		lookTarget = this.GetComponent<LookAt>();
+		//lookTarget = this.GetComponent<LookAt>();
 		player = gameObject.FindWithTag("Player");
 	}
 
 	public HideAction () {
-		addPrecondition ("hasFled", false ); 
 		addPrecondition ("needsToHide", true);
 		addEffect ("doJob", true);
 		name = "Enter hiding area!";
@@ -38,12 +37,15 @@ public class HideAction : GoapAction {
 	
 	public override bool requiresInRange ()
 	{
-		return true; 
+		return false; //Should already be in a hiding area.
 	}
 	
 	public override bool checkProceduralPrecondition (GameObject agent)
 	{	
-		lookTarget.target = player.transform;
+		//lookTarget.target = player.transform;
+		float distFromPlayer = Vector3.Distance( player.transform.position, transform.position );//Check to see if distance from player >=20 meters.
+		
+		if( distFromPlayer > 20.0f )//If far enough away from player, crouch or whatever.
 		return true;
 	}
 	
@@ -57,12 +59,12 @@ public class HideAction : GoapAction {
 			startTime = Time.time;
 		}
 
-		if (Time.time - startTime > workDuration) 
+		if (Time.time - startTime > hideDuration) 
 		{
 			Debug.Log("Finished: " + name);
 			//stock = gameObject.FindWithTag("stockpile").GetComponent<Stockpile>().fish += 1;
 			completed = true;
-			//anim.SetBool( "hiding", false );
+			anim.SetBool( "hiding", false );
 		}
 		return true;
 	}
