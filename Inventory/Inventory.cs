79a41a0ddd1ec57149fa8416fe0fcad4665b2e36
lionboy1 <39709290+lionboy1@users.noreply.github.com>
2020-03-30 15:48:43 -0400

    Update Inventory.cs
    Shows GUI - WIP

--- Inventory/Inventory.cs
+++ Inventory/Inventory.cs
@@ -1,31 +1,38 @@
-using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine;
+using RiseReign;
 
+public class Inventory : MonoBehaviour {
 
-//General inventory for all
-namespace RiseReign
-{
-	public class Inventory : Monobehaviour
+	public int flourLevel = 5;
+	public int breadLevel = 0;
+	public int wheatLevel = 0;
+	public int fishingRod = 0;
+	public int Bakes = 0;
+	public int RawFlour = 0;
+	public int Meat = 0;
+	public int Fish = 0;
+	public int Herbs = 0;
+	public int Berries = 0;
+	public int Manure = 0;
+	public int Hammer = 0;
+	public int Planks = 0;
+	public int Weapon = 0;
+	public int Nails = 0;
+	public int Coins = 0;
+	public int Trap = 0;
+	public int CaughtAnimal = 0;
+	public int cocoaBalls = 0;
+	public int CocoaTea = 0;
+	public int drawOffset = 0;
+	public string name = "";
+	
+	void OnGUI()
 	{
-		public int fishingRod = 0;
-		public int Wheat = 0;
-		public int Bakes = 0;
-		public int Flour = 0;
-		public int RawFlour = 0;
-		public int Meat = 0;
-		public int Fish = 0;
-		public int Herbs = 0;
-		public int Berries = 0;
-		public int Manure = 0;
-		public int Hammer = 0;
-		public int Planks = 0;
-		public int Weapon = 0;
-		public int Nails = 0;
-		public int Coins = 0;
-		public int Trap = 0;
-		public int CaughtAnimal = 0;
-		public int cocoaBalls = 0;
-		public int CocoaTea = 0;
+		GUI.Box(new Rect(0, 0 + drawOffset, 100, 100), "" + name);
+		GUI.Label(new Rect(10, 20 + drawOffset, 100, 20), "Flour: " + flourLevel);
+		GUI.Label(new Rect(10, 35 + drawOffset, 100, 20), "Bread: " + breadLevel);
+		GUI.Label(new Rect(10, 50 + drawOffset, 100, 20), "Wheat: " + wheatLevel);
 	}
 }
