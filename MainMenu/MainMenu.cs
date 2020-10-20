789c49343f7f409dbfd63ef95e6ea883d151a524
lionboy1 <great.studios30@gmail.com>
2020-10-20 13:11:48 -0400

    Added scene transition fading

--- MainMenu/MainMenu.cs
+++ MainMenu/MainMenu.cs
@@ -1,13 +1,29 @@
 ﻿using UnityEngine.SceneManagement;
+using System.Collections;
 using UnityEngine;
 
 public class MainMenu : MonoBehaviour
 {
+    Animator _anim;
+
+    void Start() 
+    {
+        _anim = GameObject.Find("Intro").GetComponent<Animator>();
+    }
+    
     public void LoadGame()
     {
-        SceneManager.LoadScene(1);
+        StartCoroutine("LoadLevel");
     }
 
+
+    IEnumerator LoadLevel()
+    {
+        _anim.SetTrigger("FadeIn");
+        yield return new WaitForSeconds(1.0f);
+        _anim.SetTrigger("FadeOut");
+        SceneManager.LoadScene(1);
+    }
     public void QuitGame()
     {
         Application.Quit();
