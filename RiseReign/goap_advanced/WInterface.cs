f8ca5b6a5c6969265cc1fe05df04d1d8d265b84f
lionboy1 <great.studios30@gmail.com>
2020-10-28 15:53:59 -0400

    Ability to drop in resources and fixed positioning

--- RiseReign/goap_advanced/WInterface.cs
+++ RiseReign/goap_advanced/WInterface.cs
@@ -10,6 +10,8 @@ public class WInterface : MonoBehaviour
     Vector3 goalPos;
     public NavMeshSurface surface;
     public GameObject hospital;
+    Vector3 clickOffset = Vector3.zero;
+    bool offsetCalc = false;
     void Start()
     {
         
@@ -24,15 +26,31 @@ public class WInterface : MonoBehaviour
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);//Ray from the center screeen to the position of the mouse
             if(!Physics.Raycast(ray, out hit))
                 return;
-            goalPos = hit.point;
-            focusObj = Instantiate(newResourcePrefab, goalPos, newResourcePrefab.transform.rotation);
+
+                //reset
+                offsetCalc = false;
+                clickOffset = Vector3.zero;
+            //Enable re-clicking on existing obj
+            if(hit.transform.gameObject.tag == "RestRoom")
+            {
+                focusObj = hit.transform.gameObject;
+            }
+            else
+            {
+                goalPos = hit.point;
+                focusObj = Instantiate(newResourcePrefab, goalPos, newResourcePrefab.transform.rotation);
+            }
+            focusObj.GetComponent<Collider>().enabled = false;//Disable the pickup from repeating while already holding the button down
         }
         else if(Input.GetMouseButtonUp(0))
         {
             focusObj.transform.parent = hospital.transform;//must be parented to the gameobject that has the navmesh build script
             surface.BuildNavMesh();//dynamically rebuild navmesh for new objects placed.
-            GWorld.Instance.GetQueue("toilets").AddResource(focusObj);//add the focusObject to the toilets queue(or any queue desired).
+            
+            GWorld.Instance.GetQueue("restrooms").AddResource(focusObj);//add the focusObject to the toilets queue(or any queue desired).
             GWorld.Instance.GetWorld().ModifyState("FreeToilet", 1);  //Make an additional toilet available.
+
+            focusObj.GetComponent<Collider>().enabled = true;//Allow the pick up.
         }
         else if(focusObj && Input.GetMouseButton(0))
         {
@@ -40,8 +58,24 @@ public class WInterface : MonoBehaviour
             Ray rayMove = Camera.main.ScreenPointToRay(Input.mousePosition);//Ray from the center screeen to the position of the mouse
             if(!Physics.Raycast(rayMove, out hitMove))
                 return;
-        goalPos = hitMove.point;
-        focusObj.transform.position = goalPos;
+
+            if(!offsetCalc) 
+            {
+                clickOffset = hitMove.point - focusObj.transform.position;
+                offsetCalc = true;
+            }
+
+            goalPos = hitMove.point - clickOffset;
+            focusObj.transform.position = goalPos;
+        }
+        //Rotate the objects
+        if(focusObj && (Input.GetKeyDown(KeyCode.Less) || Input.GetKeyDown(KeyCode.Comma)))
+        {
+            focusObj.transform.Rotate(0,90,0);
+        }
+        else if(focusObj && (Input.GetKeyDown(KeyCode.Greater) || Input.GetKeyDown(KeyCode.Period)))
+        {
+            focusObj.transform.Rotate(0,-90,0);
         }
     }
 }
