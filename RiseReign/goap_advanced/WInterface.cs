124a531193a1fee2b566a9517eed1fb20fd48103
lionboy1 <great.studios30@gmail.com>
2020-10-27 23:16:33 -0400

    drop and rotate resource added in WInterface script

--- RiseReign/goap_advanced/WInterface.cs
+++ RiseReign/goap_advanced/WInterface.cs
@@ -1,12 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class WInterface : MonoBehaviour
 {
     GameObject focusObj;
     public GameObject newResourcePrefab;
     Vector3 goalPos;
+    public NavMeshSurface surface;
+    public GameObject hospital;
     void Start()
     {
         
@@ -18,11 +21,27 @@ public class WInterface : MonoBehaviour
         if(Input.GetMouseButtonDown(0))
         {
             RaycastHit hit;
-            Ray ray= Camera.main.ScreenPointToRay(Input.mousePosition);//Ray from the center screeen to the position of the mouse
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);//Ray from the center screeen to the position of the mouse
             if(!Physics.Raycast(ray, out hit))
                 return;
             goalPos = hit.point;
             focusObj = Instantiate(newResourcePrefab, goalPos, newResourcePrefab.transform.rotation);
+        }
+        else if(Input.GetMouseButtonUp(0))
+        {
+            focusObj.transform.parent = hospital.transform;//must be parented to the gameobject that has the navmesh build script
+            surface.BuildNavMesh();//dynamically rebuild navmesh for new objects placed.
+            GWorld.Instance.GetQueue("toilets").AddResource(focusObj);//add the focusObject to the toilets queue(or any queue desired).
+            GWorld.Instance.GetWorld().ModifyState("FreeToilet", 1);  //Make an additional toilet available.
+        }
+        else if(focusObj && Input.GetMouseButton(0))
+        {
+            RaycastHit hitMove;
+            Ray rayMove = Camera.main.ScreenPointToRay(Input.mousePosition);//Ray from the center screeen to the position of the mouse
+            if(!Physics.Raycast(rayMove, out hitMove))
+                return;
+        goalPos = hitMove.point;
+        focusObj.transform.position = goalPos;
         }
     }
 }
